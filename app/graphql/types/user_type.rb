214a16e3a9814c675dca3dc200c810a8767f4f69
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-24 11:35:45 +0700

    show photos to user type

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,4 +4,14 @@ Types::UserType = GraphQL::ObjectType.define do
   field :id, types.ID
   field :email, types.String
   field :username, types.String
+  field :photos_count, types.Int
+
+  field :photos do
+    type types[Types::PhotoType]
+    argument :size, types.Int, default_value: 12
+
+    resolve ->(obj, args, ctx) {
+      obj.photos.limit(args[:size])
+    }
+  end
 end
