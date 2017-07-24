a94c685f3615ba2618446052cb75162873e04ef0
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-24 12:00:55 +0700

    add annotate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  photo_id   :integer
+#  content    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_comments_on_photo_id  (photo_id)
+#  index_comments_on_user_id   (user_id)
+#
+
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :photo, counter_cache: true
--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  photo_id   :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_likes_on_photo_id  (photo_id)
+#  index_likes_on_user_id   (user_id)
+#
+
 class Like < ApplicationRecord
   belongs_to :user
   belongs_to :photo, counter_cache: true
