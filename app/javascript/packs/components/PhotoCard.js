7172897d36a11d0250eb555d2937aabe57864393
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-25 15:17:27 +0700

    profile page

--- app/javascript/packs/components/PhotoCard.js
+++ app/javascript/packs/components/PhotoCard.js
@@ -8,29 +8,47 @@ const styleSheet = createStyleSheet("PhotoCard", (theme) => ({
   root: {
     marginBottom: "15px",
   },
+  block: {
+    display: "block",
+  },
 }))
 
 class PhotoCard extends React.Component {
   render() {
-    const { classes } = this.props
+    const { classes, raised, onlyMedia } = this.props
 
     return (
-      <Card className={classes.root}>
+      <Card className={classes.root} raised={raised}>
         <CardMedia>
-          <img src={"https://material-ui-1dab0.firebaseapp.com/build/abd50bc0e11052fea9669f18f0c017bc.jpg"} alt="Reptile" />
+          <img
+            src={"https://material-ui-1dab0.firebaseapp.com/build/abd50bc0e11052fea9669f18f0c017bc.jpg"}
+            alt="Reptile"
+            className={classes.block}
+          />
         </CardMedia>
-        <CardContent>
-          <Typography type="headline" component="h3">Dimas</Typography>
-          <Typography component="p">Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-            across all continents except Antarctica</Typography>
-        </CardContent>
+        {
+          onlyMedia ? null : (
+            <CardContent>
+              <Typography type="headline" component="h3">Dimas</Typography>
+              <Typography component="p">Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+                across all continents except Antarctica</Typography>
+            </CardContent>
+          )
+        }
       </Card>
     )
   }
 }
 
+PhotoCard.defaultProps = {
+  onlyMedia: false,
+  raised: false,
+}
+
 PhotoCard.propTypes = {
   classes: PropTypes.object.isRequired,
+  onlyMedia: PropTypes.bool,
+  raised: PropTypes.bool,
 }
 
 export default withStyles(styleSheet)(PhotoCard)
--- app/javascript/packs/pages/ProfilePage.js
+++ app/javascript/packs/pages/ProfilePage.js
@@ -1,10 +1,21 @@
 import React from "react"
 import { withStyles, createStyleSheet } from "material-ui/styles"
+import { Avatar, Typography, Grid } from "material-ui"
+import PropTypes from "prop-types"
+
+import PhotoCard from "../components/PhotoCard"
 
 const styleSheet = createStyleSheet("ProfilePage", (theme) => ({
   root: {
     width: "80%",
     margin: "0 auto",
+  },
+  avatar: {
+    width: 200,
+    height: 200,
+  },
+  list: {
+    flexGrow: 1,
   },
 }))
 
@@ -12,12 +23,48 @@ class ProfilePage extends React.Component {
   render() {
     const { classes } = this.props
 
+    const list = [1, 2, 3, 4, 5, 6, 7, 8, 9].map((id) => {
+      return (
+        <Grid item xs={4}>
+          <PhotoCard onlyMedia key={id} />
+        </Grid>
+      )
+    })
+
     return (
       <div className={classes.root}>
-        ProfilePage
+        <Grid container gutter={12}>
+          <Grid item xs={6}>
+            <Avatar
+              src="https://material-ui-1dab0.firebaseapp.com/build/b16427bb030d63fd8e52ea84defda1d1.jpg"
+              alt="Profile"
+              className={classes.avatar}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Typography>Photos 100</Typography>
+            <Typography>Followers 100</Typography>
+            <Typography>Followings 100</Typography>
+          </Grid>
+        </Grid>
+
+        <Grid
+          container
+          align="center"
+          direction="row"
+          justify="center"
+          gutter={24}
+          className={classes.list}
+        >
+          {list}
+        </Grid>
       </div>
     )
   }
+}
+
+ProfilePage.propTypes = {
+  classes: PropTypes.object.isRequired,
 }
 
 export default withStyles(styleSheet)(ProfilePage)
