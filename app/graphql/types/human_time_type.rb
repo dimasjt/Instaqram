633d71261112348081cf117f0ea2186cd620db27
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-27 11:50:18 +0700

    expose created_at and updated_at for photo

---
+++ app/graphql/types/human_time_type.rb
@@ -0,0 +1,21 @@
+Types::HumanTimeType = GraphQL::ObjectType.define do
+  name "HumanTime"
+
+  field :format, types.String do
+    resolve ->(time, args, ctx) {
+      time.to_date.to_formatted_s(:db)
+    }
+  end
+
+  field :human, types.String do
+    resolve ->(time, args, ctx) {
+      time.to_date.to_formatted_s(:long)
+    }
+  end
+
+  field :unix, types.Int do
+    resolve ->(time, args, ctx) {
+      time.utc.to_i
+    }
+  end
+end
--- app/graphql/types/photo_type.rb
+++ app/graphql/types/photo_type.rb
@@ -6,6 +6,8 @@ Types::PhotoType = GraphQL::ObjectType.define do
   field :comments_count, types.Int
   field :likes_count, types.Int
   field :image, Types::ImageType
+  field :created_at, Types::HumanTimeType
+  field :updated_at, Types::HumanTimeType
 
   field :comments, type: types[Types::CommentType]
   field :user, type: Types::UserType
