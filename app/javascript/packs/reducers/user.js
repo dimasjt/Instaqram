c206f4b9b014f27303517acf897eac0c2061430f
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-28 10:43:28 +0700

    null user after logout

--- app/javascript/packs/reducers/user.js
+++ app/javascript/packs/reducers/user.js
@@ -1,11 +1,14 @@
 import {
   SET_USER_BY_TOKEN,
+  USER_LOGOUT,
 } from "../constants"
 
 function user(state = null, action) {
   switch (action.type) {
     case SET_USER_BY_TOKEN:
       return action.user
+    case USER_LOGOUT:
+      return null
     default:
       return state
   }
