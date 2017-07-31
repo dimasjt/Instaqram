ef59f31ce78f70fdeee9e22c48908e9579b6d5dc
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-07-31 17:46:46 +0700

    create image

--- app/controllers/api/images_controller.rb
+++ app/controllers/api/images_controller.rb
@@ -1,4 +1,19 @@
 class Api::ImagesController < Api::BaseController
+  before_action :authenticate_user!
+
   def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      render json: { id: @image.id }, status: 201
+    else
+      render json: { errors: @image.errors }, status: 422
+    end
+  end
+
+  private
+
+  def image_params
+    params.permit(:file, :imageable_type)
   end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -16,5 +16,7 @@
 #
 
 class Image < ApplicationRecord
+  mount_uploader :file, ImageUploader
+
   belongs_to :imageable, polymorphic: true
 end
